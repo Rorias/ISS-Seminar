be0af61f25e3efe9f82a6a828b18c51f56e45a21
Lars Mulder <larsmulderrr@gmail.com>
2022-02-03 15:55:10 +0100

    added movechar option

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 tempPos = Vector2.zero;
+
+    public void MoveCharPos(Vector2 _position)
     {
-        
+        tempPos = _position;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MoveCharChar(GameObject _char)
     {
-        
+        _char.transform.position = tempPos;
     }
 }
