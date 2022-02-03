30b3d5b3d7b1f97eb9aff37861fd6895c2f4e642
Lars Mulder <larsmulderrr@gmail.com>
2022-02-03 19:37:06 +0100

    lock player when choosing

--- Assets/Scripts/ChoiceSystem.cs
+++ Assets/Scripts/ChoiceSystem.cs
@@ -11,9 +11,12 @@ public class ChoiceSystem : MonoBehaviour
     private List<GameObject> choices = new List<GameObject>();
 
     private GameObject currentMenu;
+    private PlayerMovement player;
 
     private void Awake()
     {
+        player = GameObject.Find("Player").GetComponent<PlayerMovement>();
+
         for (int i = 0; i < transform.childCount; i++)
         {
             choices.Add(transform.GetChild(i).gameObject);
@@ -30,10 +33,12 @@ public class ChoiceSystem : MonoBehaviour
         currentMenu = choices[_choiceID];
 
         currentMenu.SetActive(true);
+        player.locked = true;
     }
 
     private void CloseChoiceMenu()
     {
         currentMenu.SetActive(false);
+        player.locked = false;
     }
 }
